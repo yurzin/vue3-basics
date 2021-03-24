b39f00327585f42ce08a75913757d803ee9c5d38
Yurzin Sergey <yurzin.sergey@mail.ru>
2021-03-24 17:22:54 +0700

    second

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Счетчик'
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods: {
+        // inputChangeHandler(event) {
+        //     this.inputValue = event.target.value
+        // },
+        addNewNote() {
+            if (this.inputValue) {
+                this.notes.push(this.inputValue);
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNote(idx) {
+            this.notes.splice(idx, 1)
+        }
+    },
+    computed: {
+        doubleCounter() {
+            return this.notes.length * 2
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = ''
+            }
         }
     }
 };
